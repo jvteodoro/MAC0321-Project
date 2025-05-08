aa64ef80b5ffebd6471f612eeee54aa80c5c09b1
DarkPixel100 <diegofavila20@gmail.com>
2025-05-07 23:18:03 -0300

    Muda a estrutura do projeto gradle para se adequar ao formato de implementação de múltiplos subprojetos

---
+++ backend/src/main/java/br/com/agendusp/agendusp/controller/HomeController.java
@@ -0,0 +1,21 @@
+package br.com.agendusp.agendusp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String getHome() {
+        return new String("Hello Home");
+    }
+
+    @GetMapping("/secured")
+    public String getSecured() {
+        return new String("Hello Secured");
+    }
+    
+    
+}
