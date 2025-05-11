624e955d1cf4b7f4b09c14aba4b2d85d14ab9624
João Teodoro <joao.v.teodoro@usp.br>
2025-05-11 00:03:43 -0300

    [#97] Implementar RestClient com ClientHttpRequestInterceptor
    https://project.jteodoro.tec.br/work_packages/97
    Comunicação com google api implementada

--- backend/src/main/java/br/com/agendusp/agendusp/config/GoogleRestClientConfig.java
+++ backend/src/main/java/br/com/agendusp/agendusp/config/GoogleRestClientConfig.java
@@ -1,16 +1,8 @@
 package br.com.agendusp.agendusp.config;
-
-import java.nio.charset.StandardCharsets;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
-
-import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 
 @Configuration
 public class GoogleRestClientConfig {
