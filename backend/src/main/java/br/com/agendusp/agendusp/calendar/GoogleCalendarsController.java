3e619bece5a8c9f1ec749cea7b74932efd30ae26
João Teodoro <joao.v.teodoro@usp.br>
2025-05-12 16:03:19 -0300

    Controlador calendário google

---
+++ backend/src/main/java/br/com/agendusp/agendusp/calendar/GoogleCalendarsController.java
@@ -0,0 +1,14 @@
+package br.com.agendusp.agendusp.calendar;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+//@RestController
+public class GoogleCalendarsController {//implements CalendarsController {
+    private final RestClient restClient; 
+
+    public GoogleCalendarsController(RestClient restClient) {
+        this.restClient = restClient;   
+    }
+    //GoogleCalendarListController calendarListController = new GoogleCalendarListController(restClient);
+}
