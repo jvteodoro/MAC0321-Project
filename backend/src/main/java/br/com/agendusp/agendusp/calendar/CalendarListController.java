2e1fc996af265eb0a6f098bf13c945757889e631
gabriel <gabriel.prodossimo@usp.br>
2025-05-15 20:54:49 -0300

    Ajustes da interface do CalendarListController

--- backend/src/main/java/br/com/agendusp/agendusp/calendar/CalendarListController.java
+++ backend/src/main/java/br/com/agendusp/agendusp/calendar/CalendarListController.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 import org.springframework.http.HttpStatusCode;
 
 public interface CalendarListController {
-    //public HttpStatusCode delete();
-    public ArrayList<Calendar> get(Calendar calendar);
+    public ResponseEntity<Void> delete(String calendarId);
+    //public ArrayList<Calendar> get(Calendar calendar);
     //public CalendarList insert(Calendar calendar);
-    //public CalendarList list();
+    public CalendarList list(OAuth2AuthorizedClient authorizedClient); 
     //public CalendarList patch(Calendar calendar);
     //public CalendarList update(Calendar calendar);
     //public WatchResponse watch(WatchRequest watchRequest);
