6ec2b2bcaaac7c9045750a45fe8b7016378e2167
Sophia Soares Mariano <sophiasoaresmariano@usp.br>
2025-05-16 13:08:53 -0300

    patch method and dependencies insertion on build.gradle

--- backend/src/main/java/br/com/agendusp/agendusp/calendar/GoogleCalendarListController.java
+++ backend/src/main/java/br/com/agendusp/agendusp/calendar/GoogleCalendarListController.java
@@ -89,5 +89,64 @@ public class GoogleCalendarListController implements CalendarListController {
     //}
     public CalendarList patch(Calendar calendar){return new CalendarList()}
     public CalendarList update(Calendar calendar){return new CalendarList()}
-    public WatchResponse watch(WatchRequest watchRequest){return new WatchResponse()}*/
+    public WatchResponse watch(WatchRequest watchRequest){return new WatchResponse()}
+    //            .retrieve().toEntity(Json.class);
+    //    CalendarListResource calendarListResource = new CalendarListResource();
+    
+    // patch nao mexe no id, primary, etag, kind, accessRole, deleted (atualizar no get e no list)
+    //quem fez esse acho legal verificar eses campos
+    
+    
+    @PatchMapping("/calendar/{calendarId}")
+    public CalendarList patch(@PathVariable String calendarId, @RequestBody Calendar calendar) {
+        calendar.setId(calendarId);
+        Calendar calendarAtual = get(calendar).stream() //pega o calendario def pelo rest controller
+                .filter(c -> c.getId().equals(calendarId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("O calendário " + calendarId + " não foi encontrado."));
+
+        // atualiza o que não está sem nada
+        // patch nao mexe: id, primary, etag, kind, accessRole, deleted (atualizar no get e no list)
+        if (calendar.getSummary() != null) {
+            calendarAtual.setSummary(calendar.getSummary()); //nome
+        }
+        if (calendar.getDescription() != null) {
+            calendarAtual.setDescription(calendar.getDescription()); 
+        }
+        if (calendar.getLocation() != null) {
+            calendarAtual.setLocation(calendar.getLocation()); //localização vinculada a agenda (verificar loc de eventos)
+        }
+        if (calendar.getTimeZone() != null) {
+            calendarAtual.setTimeZone(calendar.getTimeZone()); 
+        }
+        if (calendar.getColorId() != null) {
+            calendarAtual.setColorId(calendar.getColorId());
+        }
+        if (calendar.getBackgroundColor() != null) {
+            calendarAtual.setBackgroundColor(calendar.getBackgroundColor());
+        }
+        if (calendar.getForegroundColor() != null) {
+            calendarAtual.setForegroundColor(calendar.getForegroundColor());
+        }
+        if (calendar.getHidden() != null) {
+            calendarAtual.setHidden(calendar.getHidden()); //indica se a agenda esta oculta
+        }
+        if (calendar.getSelected() != null) {
+            calendarAtual.setSelected(calendar.getSelected()); //praticamente igual ao hidden
+        }
+        if (calendar.getDefaultReminders() != null) {
+            calendarAtual.setDefaultReminders(calendar.getDefaultReminders()); //lista de lembretes (acho que é array entao verificar a implementaçao)
+        }
+        if (calendar.getConferenceProperties() != null) {
+            calendarAtual.setConferenceProperties(calendar.getConferenceProperties()); //google meet
+        }
+        return update(calendarAtual); 
+        //update conforme orientação do google api
+    }
+
+
+
+    /* 
+    public CalendarList update(Calendar calendar){}
+    public WatchResponse watch(WatchRequest watchRequest){}*/
 }
