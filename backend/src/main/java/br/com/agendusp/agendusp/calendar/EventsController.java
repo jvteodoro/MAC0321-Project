aef89bf65f24c2b97034bfd2b2bc81fee4d092c0
DarkPixel100 <diegofavila20@gmail.com>
2025-05-30 02:07:28 -0300

    Implementa métodos list de CalendarListController e de EventsController. Adiciona alguns mappings para receber requisições.

--- backend/src/main/java/br/com/agendusp/agendusp/calendar/EventsController.java
+++ backend/src/main/java/br/com/agendusp/agendusp/calendar/EventsController.java
@@ -1,17 +1,30 @@
 package br.com.agendusp.agendusp.calendar;
 
 import org.springframework.http.HttpStatusCode;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 
 public interface EventsController {
-    public HttpStatusCode delete(String calendarId, String eventId);
-    public String get(String calendarId, String eventId);
+    public HttpStatusCode delete(String calendarId, String eventId, OAuth2AuthorizedClient authorizedClient);
+
+    public String get(String calendarId, String eventId, OAuth2AuthorizedClient authorizedClient);
+
     // public String importEvent(String calendarId, EventResource event);
-    public String insert(String calendarId, EventsResource event);
+
+    public String insert(String calendarId, EventsResource event, OAuth2AuthorizedClient authorizedClient);
+
     // public CalendarEvents instances(String calendarId, String eventId);
-    public String list(String calendarId);
+
+    public String list(String calendarId, OAuth2AuthorizedClient authorizedClient);
+
     // public String move(String calendarId, String eventId, String destination);
-    public String patch(String calendarId, String eventId, EventsResource event);
+
+    public String patch(String calendarId, EventsResource event,
+            OAuth2AuthorizedClient authorizedClient);
+
     // public String quickAdd(String calendarId, String text);
-    public String update(String calendarId, String eventId, EventsResource event);
+
+    public String update(String calendarId, EventsResource event,
+            OAuth2AuthorizedClient authorizedClient);
+
     // public WatchResponse watch(String calendarId, WatchRequest watchRequest);
 }
