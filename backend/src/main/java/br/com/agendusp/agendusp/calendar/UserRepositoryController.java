f711a1e5e5f4a27661ee13c19b3ae367ee0316ba
João Teodoro <joao.v.teodoro@usp.br>
2025-05-31 23:24:56 -0300

    Cria controlador do repositório de usuários

---
+++ backend/src/main/java/br/com/agendusp/agendusp/calendar/UserRepositoryController.java
@@ -0,0 +1,28 @@
+package br.com.agendusp.agendusp.calendar;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import br.com.agendusp.agendusp.documents.User;
+import br.com.agendusp.agendusp.repositories.UserRepository;
+
+public class UserRepositoryController {
+    @Autowired
+    private final UserRepository userRepository;
+    @Autowired
+    private final OAuth2User oAuth2User;
+
+    public UserRepositoryController(OAuth2User oAuth2User, UserRepository userRepository) {
+        this.oAuth2User = oAuth2User;
+        this.userRepository = userRepository;
+    }
+    public void addUser(){
+        User user = new User();
+        user.setUsername(oAuth2User.getName());
+        user.setEmail(oAuth2User.getAttributes().get("email").toString());
+        user.setGoogleId(oAuth2User.getAttribute("sub")); // 'sub' é o ID estável do usuário no Google
+        userRepository.insert(user);
+    }
+}
