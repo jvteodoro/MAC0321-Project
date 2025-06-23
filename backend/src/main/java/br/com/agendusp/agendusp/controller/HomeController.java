25ea8b9270fb22dce6b3ed6697892e3e4d31b268
João Teodoro <joao.v.teodoro@usp.br>
2025-06-22 21:43:35 -0300

    implementa estatísticas básicas

--- backend/src/main/java/br/com/agendusp/agendusp/controller/HomeController.java
+++ backend/src/main/java/br/com/agendusp/agendusp/controller/HomeController.java
@@ -1,12 +1,46 @@
 package br.com.agendusp.agendusp.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.agendusp.agendusp.dataobjects.DateTimeInterval;
+import br.com.agendusp.agendusp.documents.EventsResource;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class HomeController {
+
+    @Autowired
+    EventsDataController eventsDataController;
+
+    @GetMapping("/stats")
+    public String generateStats(@RequestBody DateTimeInterval interval) {
+        ArrayList<EventsResource> allEvents = eventsDataController.getEventsOnInterval(interval);
+        int eventsNum = allEvents.size();
+        long timeInEvents = 0;
+        int canceledEvents = 0;
+        for (EventsResource ev: allEvents){
+            timeInEvents += ChronoUnit.HOURS.between(ev.getEnd().getDateTime(), ev.getStart().getDateTime());
+            if (ev.getStatus() == "cancelled"){
+                canceledEvents += 1;
+            }
+        }
+
+        return "Você participou de "+eventsNum+
+        "eventos, o que corresponde a "+timeInEvents+
+        " horas"+". Desses eventos, "+canceledEvents+" foram cancelados.";
+    }
+    
 
     @GetMapping("/")
     public String getHome() {
